e3d071c9777c2821930ed5174a99c5b12849fca7
Benjamin Huh <benjamin.s.huh@gmail.com>
2020-01-09 17:38:31 -0800

    Add animation to front page

--- frontend/components/main/phone/phone.jsx
+++ frontend/components/main/phone/phone.jsx
@@ -1,8 +1,22 @@
 import React from 'react'
+import ReactDOM from 'react-dom'
 
-export default Phone = () => (
+class Phone extends React.Component {
 
-    <div>
-    
-    </div>
-)
+    render() {
+        return (
+            <div className="phone-container">
+                <video className="phone" autoPlay loop muted preload="auto">
+                    <source src={window.phonescreen} type="video/mp4" />
+                </video>
+
+                <div className="phone-frame-div">
+                    <img className="phone-frame" src={window.mainpagephone} />
+                </div> 
+
+            </div>
+        );
+    }
+}
+
+export default Phone;
