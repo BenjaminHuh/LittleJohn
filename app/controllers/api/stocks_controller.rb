76ba22194ad3161fd148bade77b523b7374e2949
Benjamin Huh <benjamin.s.huh@gmail.com>
2020-01-09 16:37:01 -0800

    Cleared up stock JSON

--- app/controllers/api/stocks_controller.rb
+++ app/controllers/api/stocks_controller.rb
@@ -3,7 +3,7 @@ class Api::StocksController < ApplicationController
         @stock = Stock.find_by_ticker(params[:ticker])
         ticker = @stock.ticker
         stock_url = "https://query1.finance.yahoo.com/v7/finance/quote?symbols=#{ticker}"
-        @stock_info = HTTParty.get(stock_url).parsed_response
+        @stock_info = HTTParty.get(stock_url).parsed_response["quoteResponse"]["result"][0]
 
     end
     
