42a18c639957ea5f11c6cba703360f83446673bb
BenjaminHuh <benjamin.s.huh@gmail.com>
2020-01-12 19:00:43 -0800

    Revise front page

--- frontend/components/navbar/navbar_container.js
+++ frontend/components/navbar/navbar_container.js
@@ -3,8 +3,9 @@ import { connect } from 'react-redux';
 import { logout } from '../../actions/session_actions';
 import Navbar from './navbar';
 
-const mSTP = ({ session, entities: { users } }) => ({
-    currentUser: users[session.id]
+const mSTP = ({ session, entities: { users, stock } }) => ({
+    currentUser: users[session.id],
+    stock
 });
 
 const mDTP = dispatch => ({
