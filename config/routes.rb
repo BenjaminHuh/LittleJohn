9cff7331bf3abb9d465e647a841968f01194807c
Benjamin Huh <benjamin.s.huh@gmail.com>
2020-01-16 12:31:57 -0800

    Add ticker change status

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
   namespace :api, defaults: { format: :json } do
-    resources :users
+    resources :users, only: [:create, :show] do
+      resources :accounts, only: [:create, :show]
+    end
+
     resource :session, only: [:create, :destroy, :show]
     resources :stocks, only: [:show], param: :ticker
     resources :stocks, only: [:index]
--- frontend/components/portfolio/watchlist_item.jsx
+++ frontend/components/portfolio/watchlist_item.jsx
@@ -2,15 +2,40 @@ import React from 'react';
 import { Link } from 'react-router-dom'
 
 class WatchlistItem extends React.Component {
+    constructor(props) {
+        super(props);
+        this.ticker = {
+            color: "black",
+            change: 0 
+        }
+    }
     componentDidMount() {
         this.props.getWatchlistItem(this.props.id)
-        this.interval = setInterval(() => this.props.getWatchlistItem(this.props.id), 10000);
+        this.interval = setInterval(() => this.props.getWatchlistItem(this.props.id), 2000);
     }
 
     componentWillUnmount() {
         clearInterval(this.interval);
     }        
 
+    componentDidUpdate(prevState) {
+        if (prevState.stock.info !== undefined && this.props.stock.info !== undefined) {
+            let prevPrice = prevState.stock.info.regularMarketPrice;
+            let newPrice = this.props.stock.info.regularMarketPrice;
+            let change = (prevPrice - newPrice) / prevPrice * 100;
+            if (change > 0) {
+                this.ticker = { 
+                    color: "price-green",
+                    change: change
+                 }
+            } else {
+                this.ticker = { 
+                    color: "price-red",
+                    change: change
+                    }
+            }
+        }
+    }    
 
     render() {
         
@@ -18,17 +43,20 @@ class WatchlistItem extends React.Component {
             return null;
         } 
         else {
+            let ticker_color = this.ticker.color;
+            let ticker_change = this.ticker.change;
             const { symbol, regularMarketPrice } = this.props.stock.info;
             return (
                 <Link className="port-item-link" to={`/stocks/${symbol}`}>
                     <div className="port-item-symbol"> 
                         <div></div>
-                        <div>{symbol}</div>
+                        <div className={ticker_color}>{symbol}</div>
                         <div></div>
                     </div>
                     <div className="port-item-chart"></div>
                     <div className="port-item-price">
-                        <div>${regularMarketPrice}</div>
+                        <div className={ticker_color}>${regularMarketPrice.toFixed(2)}</div>
+                        <div className={ticker_color}>{(ticker_change).toFixed(2)}%</div>
                     </div>
                     
                 </Link> 
