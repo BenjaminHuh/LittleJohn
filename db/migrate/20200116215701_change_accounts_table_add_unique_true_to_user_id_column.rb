3fe58a585aba18330e1f0c84b58b3dd402852b8d
Benjamin Huh <benjamin.s.huh@gmail.com>
2020-01-17 14:12:57 -0800

    Fix migration

--- db/migrate/20200116215701_change_accounts_table_add_unique_true_to_user_id_column.rb
+++ db/migrate/20200116215701_change_accounts_table_add_unique_true_to_user_id_column.rb
@@ -1,5 +1,5 @@
 class ChangeAccountsTableAddUniqueTrueToUserIdColumn < ActiveRecord::Migration[5.2]
   def change
-    change_column :accounts, :user_id, unique: true
+    add_index :accounts, :user_id, unique: true
   end
 end
