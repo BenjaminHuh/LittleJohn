81ebed543f1d2950c4c3d016d5c832c990e04c1e
Benjamin Huh <benjamin.s.huh@gmail.com>
2020-02-17 18:40:37 -0800

    Revise Chart container

--- frontend/reducers/stock_reducer.js
+++ frontend/reducers/stock_reducer.js
@@ -2,11 +2,11 @@ import {
     RECEIVE_STOCK
 } from '../actions/stocks_actions'
 
-const stockReducer = (state = { NASDAQ: { symbol: "NASDAQ" } }, action) => {
+const stockReducer = (state = { NASDAQ: { symbol: "NASDAQ", data: [] } }, action) => {
     Object.freeze(state);
     switch (action.type) {
         case RECEIVE_STOCK:
-            const newStock = { [action.stock.symbol]: action.stock }
+            const newStock = { [action.stock.symbol]: action.stock, data: action.stock.data }
             // debugger
             return Object.assign({}, newStock);
 
